f803102fcca60494e303517e35149c5800d7c403
Otto Bittner <cobittner@posteo.net>
2024-01-22 10:17:45 +0100

    add init example
    this breaks kernel logging.

---
+++ cuInit/cuInit.cu
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#include <cuda.h>
+
+/**
+* Host main routine
+*/
+int main(void) {
+  // Error code to check return values for CUDA calls
+  CUresult cudaSuccess = CUDA_SUCCESS;
+  // int cuDevice = 0;
+  // CUcontext *cuCtx = NULL;
+
+  printf("cuInit\n");
+
+  CUresult err = cuInit(0);
+  if (err != cudaSuccess) {
+    const char *ret = NULL;
+    cuGetErrorString(err, &ret);
+    fprintf(stderr, "error: %s\n", ret);
+    exit(EXIT_FAILURE);
+  }
+
+//   printf("cuDeviceGet\n");
+
+//   err = cuDeviceGet(&cuDevice, 0);
+//   if (err != cudaSuccess) {
+//     const char *ret = NULL;
+//     cuGetErrorString(err, &ret);
+//     fprintf(stderr, "error: %s\n", ret);
+//     exit(EXIT_FAILURE);
+//   }
+
+//   printf("cuDevicePrimaryCtxRetain\n");
+
+//   err = cuDevicePrimaryCtxRetain(cuCtx, cuDevice);
+//   if (err != cudaSuccess) {
+//     const char *ret = NULL;
+//     cuGetErrorString(err, &ret);
+//     fprintf(stderr, "error: %s\n", ret);
+//     exit(EXIT_FAILURE);
+//   }
+
+
+  printf("Done\n");
+  return 0;
+}
